caf0d14de37ccbd7cad2f2adea0dfc47e84ec36f
UlianaMinaieva <uminaieva@gmail.com>
2016-08-11 23:32:32 +0300

    secont Commit

--- src/test/java/lesson2.java
+++ src/test/java/lesson2.java
@@ -1,5 +1,14 @@
+import org.testng.annotations.Test;
+
 /**
  * Created by pavilion on 11.08.2016.
  */
-public class lesson2 {
+public class test2 {
+
+    @Test
+    public void testName() throws Exception
+    {
+
+    }
+
 }
