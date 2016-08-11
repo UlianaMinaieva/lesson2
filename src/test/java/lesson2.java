dbc8431a838c3355df04a0ff716c309b0f028196
UlianaMinaieva <uminaieva@gmail.com>
2016-08-11 23:58:26 +0300

    secont Commit

--- src/test/java/lesson2.java
+++ src/test/java/lesson2.java
@@ -1,3 +1,7 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 /**
@@ -8,6 +12,19 @@ public class lesson2 {
     @Test
     public void testName() throws Exception
     {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://finance.i.ua/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("fn_s1")).sendKeys("2000");
+        new Select(driver.findElement(By.id("fn_s1"))).selectByVisibleText("USD");
+        new Select(driver.findElement(By.id("fn_bank"))).selectByVisibleText("НБУ");
+
+
+
+
+
+        driver.quit();
 
     }
 
