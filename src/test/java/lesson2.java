d8cf23553c625e5b4b75b3b16f671d28c7dc1595
UlianaMinaieva <uminaieva@gmail.com>
2016-08-13 23:23:04 +0300

    secont Commit

--- src/test/java/lesson2.java
+++ src/test/java/lesson2.java
@@ -1,8 +1,14 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by pavilion on 11.08.2016.
@@ -17,12 +23,10 @@ public class lesson2 {
         driver.manage().window().maximize();
 
         driver.findElement(By.id("fn_s1")).sendKeys("2000");
-        new Select(driver.findElement(By.id("fn_s1"))).selectByVisibleText("USD");
+        new Select(driver.findElement(By.id("fn_c1"))).selectByVisibleText("USD");
         new Select(driver.findElement(By.id("fn_bank"))).selectByVisibleText("НБУ");
 
-
-
-
+        Assert.assertEquals(driver.findElement(By.id("fn_o1_1")).getAttribute("value"),"49 700,82");
 
         driver.quit();
 
